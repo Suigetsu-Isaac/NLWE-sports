5abc03357dca33c22157a8703755605cce265d4b
Suigetsu-Isaac <isaacantonioalves@gmail.com>
2023-05-23 16:18:29 -0300

    consertando o modal

--- src/components/CreateAdModal.tsx
+++ src/components/CreateAdModal.tsx
@@ -58,7 +58,7 @@ export default function CreateAdModal() {
   return (
     <Dialog.Portal>
       <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
-      <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
+      <Dialog.Content className="fixed bg-[#2A2634] sm:py-8 py-6 sm:px-10 px-8 text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg sm:w-[480px] w-[400px] shadow-lg shadow-black/25">
         <Dialog.Title className="text-3xl font-black ">
           Publique um anúncio
         </Dialog.Title>
@@ -91,7 +91,7 @@ export default function CreateAdModal() {
             <Input id="name" name="name" placeholder="Como te chamam dentro do game?" />
           </div>
 
-          <div className="grid grid-cols-2 gap-6">
+          <div className="grid sm:grid-cols-2 grid-cols-1 sm:gap-3">
             <div className="flex flex-col gap-2">
               <label htmlFor="yearsPlaying">Joga a quantos anos?</label>
               <Input
